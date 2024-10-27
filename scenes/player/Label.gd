fd87ad9c742cedfa5095aa1f5a5f7fc1db795bb4
dipori <oriol.carrascosa@gmail.com>
2024-10-27 23:04:08 +0100

    l

---
+++ scenes/player/Label.gd
@@ -0,0 +1,11 @@
+extends Label
+
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#$countdown.start()
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#$jugador/Label.txt = str($countdown.time_left)
